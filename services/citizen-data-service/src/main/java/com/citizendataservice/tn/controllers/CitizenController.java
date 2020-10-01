a4cceaf6f338ab33980dd71df8129306ecca495a
MedAmine-SUDO <mohamedamine.benafia@esprims.com>
2020-10-01 13:21:27 +0100

    Trigger heroku build for citizen data service

--- services/citizen-data-service/src/main/java/com/citizendataservice/tn/controllers/CitizenController.java
+++ services/citizen-data-service/src/main/java/com/citizendataservice/tn/controllers/CitizenController.java
@@ -21,6 +21,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+//Citizen data service
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/citizen")
